60bb1570b6cbc66c6aa3c8b6dbd0a563bff9b041
Nanz Nair <nanz89@gmail.com>
2019-03-28 20:08:09 +1300

    refactor 2

--- index.js
+++ index.js
@@ -5,17 +5,11 @@ const d = require('./data.json')
 
 
 const sorted = d.wait.sort(function(a,b) {
-     
-    // make random number 
-    var randomNumber = Math.random()
-
-    // if random number is bigger than .5 return -1 
-    if (randomNumber > 0.5) {
+    if (Math.random() > 0.5) {
         return -1
     } else {
         return 1
     }
-
 })
 
 
